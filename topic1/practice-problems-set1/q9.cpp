2e599cf560584ce6777b08d02054c39cedb65dab
monpeco <monpeco@gmail.com>
2017-04-25 02:47:39 +0000

    Add second set of exercises (q9, q10, q11)

---
+++ topic1/practice-problems-set1/q9.cpp
@@ -0,0 +1,16 @@
+// For all values of 'm' and 'n', no "!" is printed.
+
+#include <iostream>
+using namespace std; 
+int main ()
+{
+  int m, n, temp; 
+  cout << "Give integers m and n: "; 
+  cin >> m >> n; 
+  do { 
+    if ( m < n) n = n - m; 
+    if ( n < m) {temp = m; m = n; n = temp;}
+    if ( m > n)cout << "!"; 
+  } while ( m < n); 
+  return 0; 
+}
